5bc552163d4dbb9c1097a519c36cd0d96d1312aa
Levva01 <vellanifederico13@gmail.com>
2022-06-09 15:30:23 +0200

    Milestone 1 completata

--- src/components/blocks/BaseHeader.vue
+++ src/components/blocks/BaseHeader.vue
@@ -1,13 +1,48 @@
 <template>
-  <header></header>
+  <header>
+
+      <form @submit.prevent="submit()">                     
+          <input class="" type="text" placeholder="Inserisci il titolo" v-model="searchBar">
+          <button type="submit">Invia</button>                 
+      </form>
+
+  </header>
 </template>
 
 <script>
+
+import axios from 'axios';
+import search from "../../shared/search";
+
 export default {
     name: 'BaseHeader',
+    data(){
+        return {
+            searchBar: '',
+            search
+        }
+    },
+
+     methods: {
+        submit() {
+            axios.get('https://api.themoviedb.org/3/search/movie',
+                {
+                    params: {
+                        api_key: 'a7a419e37d72e1c36a8e9d8a86beb8d5',
+                        query: this.searchBar,
+                        language: 'it-IT'
+                    }
+                }
+            ).then((response) => {
+                search.films = response.data.results;
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -1,13 +1,29 @@
 <template>
-  <main></main>
+  <main>
+    <ul>
+        <li class="" v-for="film in search.films" :key="film.id">{{film.title}} , {{film.original_title}} , {{film.original_language}} , {{film.vote_average}}</li>
+    </ul>
+  </main>
 </template>
 
+
 <script>
+
+import search from "../../shared/search";
+
 export default {
     name: 'BaseMain',
+    components: {
+    },
+    data(){
+        return {
+            search
+        }
+    },
+    
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
